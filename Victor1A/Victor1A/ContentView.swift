cdba437ce92e3c95a1d5b93c75817787bd13a07f
samsithuwin <150372508+samsithuwin@users.noreply.github.com>
2024-03-14 10:32:16 -0400

    Add files via upload

---
+++ Victor1A/Victor1A/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Victor1A
+//
+//  Created by sam on 3/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Victor1A/Victor1A/Victor1AApp.swift
@@ -0,0 +1,17 @@
+//
+//  Victor1AApp.swift
+//  Victor1A
+//
+//  Created by sam on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Victor1AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
